ed5eff6695f364b3612b549bbb3a26cc7a765b2a
Fernando HBS <fernandobailao@gmail.com>
2022-01-29 02:23:44 -0300

    Add files via upload

---
+++ desafio_tabuada.py
@@ -0,0 +1,35 @@
+from time import sleep
+n = int(input('Digite um número para a sua TABUADA: '))
+a = n * 1
+b = n * 2
+c = n * 3
+d = n * 4
+e = n * 5
+f = n * 6
+g = n * 7
+h = n * 8
+i = n * 98
+j = n * 10
+print('-'*39)
+print('Caulculando... ')
+print('-'*39)
+sleep(2)
+print(f'A tabuada do número {n} é: ')
+sleep(1)
+print('-'*39)
+print(f'{n} x  1 = { a:2} ')
+print(f'{n} x  2 = { b:2} ')
+print(f'{n} x  3 = { c:2} ')
+print(f'{n} x  4 = { d:2} ')
+print(f'{n} x  5 = { e:2} ')
+print(f'{n} x  6 = { f:2} ')
+print(f'{n} x  7 = { g:2} ')
+print(f'{n} x  8 = { h:2} ')
+print(f'{n} x  9 = { i:2} ')
+print(f'{n} x 10 = { j:2} ')
+print('-'*39)
+
+
+
+
+
